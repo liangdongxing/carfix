8c4eb00a51515c3e8b80fd481a47f6bd76acf7e6
vagrant <vagrant@homestead>
2019-02-02 09:05:39 +0000

    static html file browser caching problem

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
     <div class="container">
         <!-- Branding Image -->
+        <img src="{{asset('css/logo.png')}}" style="height: 35px"/>
         <a class="navbar-brand " href="{{ url('/') }}">
-            汽修宝
+
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') -  汽修宝</title>
+    <title>@yield('title', 'LaraBBS') - 汽修宝</title>
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
